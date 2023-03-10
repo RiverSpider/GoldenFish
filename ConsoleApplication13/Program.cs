00286352a56260c86fc8db1c66605e7e0b0e46e6
RiverSpider <raevskih8888@mail.ru>
2023-03-10 18:35:15 +0600

    reworked

--- ConsoleApplication13/Program.cs
+++ ConsoleApplication13/Program.cs
@@ -26,31 +26,8 @@ namespace ConsoleApplication13
                 string[] a = Console.ReadLine().Split();
                 start[i, 0] = a[0];
                 start[i, 1] = a[1];
-                /* for (int j = 0; j < n;j++)
-                 {
-                     char[] y = words[j].ToCharArray();
-                     if (y[0] != Convert.ToChar(start[i,0])){
-                         words.RemoveAt(j);
-                         n = n - 1;
-                     }
-                 }*/
             }
-            /*(не работает)for (int write = 0; write < start.Length/2 - 1; write++)
-            {
-                for (int sort = 0; sort < start.Length/2 - 2; sort++)
-                {
-                    if (Convert.ToInt32(Convert.ToChar(start[sort, 0])) > Convert.ToInt32(Convert.ToChar(start[sort + 1,0])))
-                    {
-                        string temp1 = start[sort + 1, 0];
-                        string temp2 = start[sort + 1, 1];
-                        start[sort + 1, 0] = start[sort, 0];
-                        start[sort, 0] = temp1;
-                        start[sort + 1, 1] = start[sort, 1];
-                        start[sort, 1] = temp2;
-                    }
-                }
-            }*/
-
+            
             int w = Convert.ToInt32(Console.ReadLine());
             string[,] end = new string[w, 2];
             for (int i = 0; i < w; i++)
@@ -58,31 +35,8 @@ namespace ConsoleApplication13
                 string[] a = Console.ReadLine().Split();
                 end[i, 0] = a[0];
                 end[i, 1] = a[1];
-                /*for (int j = 0; j < n; j++)
-                {
-                    char[] y = words[j].ToCharArray();
-                    if (y[words.Count()] != Convert.ToChar(end[i, 0]))
-                    {
-                        words.RemoveAt(j);
-                        n = n - 1;
-                    }
-                }*/
             }
-            /*(не работает)for (int write = 0; write < end.Length/2-1; write++)
-            {
-                for (int sort = 0; sort < end.Length/2 - 2; sort++)
-                {
-                    if (Convert.ToInt32(Convert.ToChar(end[sort, 0])) > Convert.ToInt32(Convert.ToChar(end[sort + 1, 0])))
-                    {
-                        string temp1 = end[sort + 1, 0];
-                        string temp2 = end[sort + 1, 1];
-                        end[sort + 1, 0] = end[sort, 0];
-                        end[sort, 0] = temp1;
-                        end[sort + 1, 1] = end[sort, 1];
-                        end[sort, 1] = temp2;
-                    }
-                }
-            }*/
+            
             //--------------------------------------------------------------------------
             int counter = 0;
             for (int i = 0; i<n;i++)
@@ -95,14 +49,16 @@ namespace ConsoleApplication13
                     if (y[0] == Convert.ToChar(start[j,0]) && Convert.ToInt32(start[j,1])!=0)
                     {
                         k = j;
+                        break;
                     }
                 }
                 for (int o = 0; o<w; o++)
                 {
                     char[] y = words[i].ToArray();
-                    if (y[y.Length-1] == Convert.ToChar(end[o, 0]) && Convert.ToUInt32(end[0,1])!=0)
+                    if (y[y.Length-1] == Convert.ToChar(end[o, 0]) && Convert.ToInt32(end[o,1])!=0)
                     {
                         t = o;
+                        break;
                     }
                 }
                 if (k!=-1 && t!=-1)
